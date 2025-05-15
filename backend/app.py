e96c3077184975a18f1b6d4b64e00291ceadefd3
unknown <khandelwalbhavya2703@gmail.com>
2025-05-15 13:58:41 +0530

    New Commit

--- backend/app.py
+++ backend/app.py
@@ -31,7 +31,7 @@ CORS(app)  # Enable CORS for all routes
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 with open("../shared/abi.json", "r") as f:
     contract_abi = json.load(f)
-contract_address = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+contract_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 contract = w3.eth.contract(address=contract_address, abi=contract_abi)
 
 # Encryption key
